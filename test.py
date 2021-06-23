39e92eaf113164ab44226e67fbabc8104e0f63d5
unknown <ilya.smirnov@simbirsoft.com>
2021-06-23 13:50:30 +0300

    fix for tests

--- test.py
+++ test.py
@@ -1,21 +1,19 @@
 import unittest
-from cipher import Cipher
+from cipher import Caesar
+
 
 class TestCipher(unittest.TestCase):
+    text = 'the quick brown fox jumps over the lazy dog'
 
-    def test_caesar_no_overflow(self):
-        text = 'abcd'
-        n = 2
-        correct = 'cdef'
+    def test_caesar_encode(self):
+        encoded = Caesar.encode(self.text)
+        self.assertNotEqual(encoded, self.text)  # Закодированный текст отличается от исходого
+        self.assertEqual(len(encoded), len(self.text))  # Длина закодированного и исходного текста совпадает
 
-        result = Cipher.caesar(text, n)
-        self.assertEqual(correct, result)
-
-    def test_caesar_overflow(self):
-        text = 'abcd'
-        n = 25
-
-        result = Cipher.caesar(text, n)
+    def test_caesar_decode(self):
+        encoded = Caesar.encode(self.text)
+        decoded = Caesar.decode(encoded)
+        self.assertEqual(decoded, self.text)  # Раскодированный шифр совпадает с исходным текстом
 
 
 if __name__ == '__main__':
